c4c4f54b5dc6ca493320f96003845e0a9b941555
lienowen <lienowen@outlook.com>
2026-07-07 17:53:45 +0800

    style: load v2 root and planogram css only in v2

--- src/supermarket-v2/SupermarketV2Entry.jsx
+++ src/supermarket-v2/SupermarketV2Entry.jsx
@@ -1,3 +1,5 @@
+import "./replenishment-root.css";
+import "./replenishment-planogram.css";
 import { ReplenishmentShiftGame } from "./ReplenishmentShiftGame.jsx";
 
 export function SupermarketV2Entry() {
