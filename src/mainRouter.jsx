dbbceae5b5a937b815dfde9cec87cd78a04a1b8d
lienowen <lienowen@outlook.com>
2026-07-07 17:54:27 +0800

    refactor: isolate all v2 css inside v2 entry

--- src/mainRouter.jsx
+++ src/mainRouter.jsx
@@ -1,7 +1,6 @@
 import React, { Suspense, lazy } from "react";
 import { createRoot } from "react-dom/client";
 import { redirectToLocaleIfNeeded } from "./i18n/locale.js";
-import "./supermarket-v2/replenishment-planogram.css";
 
 const params = new URLSearchParams(window.location.search);
 const forceLegacy = params.get("legacy") === "true"
